5cdacf37e58073bb29ecdf6c9058cb16408a0371
Jane Svintsova <jane.karell19@gmail.com>
2025-03-26 11:54:23 +0000

    Add the candidate page, types of interview, roles

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewPortal.Controllers;
+[Authorize(Roles = "Candidate")]
+public class CandidateController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult ApplyPosition(int id)
+    {
+        return View();
+    }
+
+    [Microsoft.AspNetCore.Mvc.Route("Candidate/ApplyPosition/{id}/{type}")]
+    public IActionResult ApplyPositionWithType(int id, string type)
+    {
+        ViewData["PositionId"] = id;
+        ViewData["InterviewType"] = type;
+
+        return View("InterviewStart");
+    }
+}
