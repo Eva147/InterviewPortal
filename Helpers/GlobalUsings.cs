be7de55b0cbb2ac3512af4dea4efab48d697b8cd
Sam Winther <samwinther24@gmail.com>
2025-04-04 04:31:52 +0100

    - Added CRUD for Positions & Topics - Updated GlobalUsings - Admin & HR role has access to everything a Candidate has (for testing purposes) - Index file accomodates for backend features

--- Helpers/GlobalUsings.cs
+++ Helpers/GlobalUsings.cs
@@ -12,6 +12,6 @@ global using System.Diagnostics;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Authorization;
-global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using System.Security.Claims;
