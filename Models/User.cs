af8fd0c236766f4ee7917d1ce449b995404d59d3
Sam Winther <samwinther24@gmail.com>
2025-04-24 19:27:52 +0100

    removed comment

--- Models/User.cs
+++ Models/User.cs
@@ -3,7 +3,6 @@ public class User : IdentityUser
 {
     public string FirstName { get; set; } = String.Empty;
     public string LastName { get; set; } = String.Empty;
-    // do we need all this information?
     public ICollection<Result>? Results { get; set; }
     public ICollection<UserAnswer>? UserAnswers { get; set; }
     public ICollection<InterviewSession>? InterviewSessions { get; set; }
